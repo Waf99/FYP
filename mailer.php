fb1da7d31925f8c2cfeb563fb6f4ca0011990d59
GG <wafrisyahiran@gmail.com>
2025-12-10 15:02:17 +0800

    Add files via upload
    Signed-off-by: GG <wafrisyahiran@gmail.com>

---
+++ mailer.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require __DIR__ . "/vendor/autoload.php";
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER; // Comment out after testing
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->Username = "benndover997@gmail.com";   // Your Gmail
+$mail->Password = "rkbjklftyqlrsnji";  // App Password (NO SPACES!)
+
+$mail->isHtml(true);
+
+return $mail;
